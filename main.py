5e956987e61d9fa56120d21586dbc9fd959e6c9b
Dalen Ledingham <dalen@Dalens-MacBook-Pro.local>
2021-08-29 17:29:59 -0500

    Spend function split into smaller functions

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from typing import List, Dict, Optional
 from datetime import datetime
 import json
 
+from pydantic.fields import T
+
 class Payer(BaseModel):
     name: str
     points: int = 0
@@ -62,53 +64,45 @@ async def create_transaction(transaction: Transaction):
 async def get_all_transactions():
     return transactions
 
-global global_points
-global_points = 0
+
 # Spend Route
+
+global spent_points
+spent_points = []
 
 @app.post("/spend/")
 async def spend(points_to_spend: int):
     global user_points
-    global spent_points
-    global global_points
-    global_points = points_to_spend
-    spent_points = {}
+
+    if points_to_spend > user_points:                                                               # if the user tries to spend more points than they have available
+        raise HTTPException(status_code=400, detail="Not enough points")
+
     sorted_transactions = sorted(transactions, key=lambda x: x.timestamp) # sort transactions by timestamp
-
-    # if the user tries to spend more points than they have available
-    if points_to_spend > user_points:
-        raise HTTPException(status_code=400, detail="You do not have enough points")
     
-    # for each transaction in sorted_transactions
     for transaction in sorted_transactions:
-        # if there are points left to spend
-        if global_points > 0 and transaction.spent is False:
-        
-            # if there are more points to spend than this transaction has available
-            if global_points >= transaction.remaining_points:
-                # if this transaction's payer has more points than this transaction has available - SPEND ALL TRANSACTION
-                if payers[transaction.payer] >= transaction.remaining_points:
-                    global_points = spend_all_transaction_points(global_points, transaction)
+        if points_to_spend > 0 and transaction.spent is False:                                      # if there are points left and this transaction is not spent
 
-                # if this transaction has more points than its payer has available - SPEND ALL PAYER
-                else:
-                    global_points = spend_all_payer_points(global_points, transaction)
+            if points_to_spend >= transaction.remaining_points:                                     # if there are more points left than this transaction has left
 
-            # if this transaction has more points than there are left to spend
-            else:
-                # if this transactin's payer has more points than this transaction has available - SPEND ALL REMAINING
-                if payers[transaction.payer] >= transaction.remaining_points:
-                    global_points = spend_all_user_points(global_points, transaction)
+                if payers[transaction.payer] >= transaction.remaining_points:                       # if payer has more points than transaction
+                    points_to_spend = spend_all_transaction_points(points_to_spend, transaction)
+                    add_spent_points(transaction.payer, transaction.points)
 
-                # if this transaction has more points than there are left to spend
-                else:
-                    # if this transaction's payer has more points than there are left to spend - SPEND ALL REMAINING
-                    if payers[transaction.payer] >= global_points:
-                        global_points = spend_all_user_points(global_points, transaction)
+                else:                                                                               # if this transaction has more points than payer has left
+                    points_to_spend = spend_all_payer_points(points_to_spend, transaction)
+                    add_spent_points(transaction.payer, payers[transaction.payer])
 
-                    # if there are more points left to spend than this transaction's payer has available - SPEND ALL PAYER
-                    else:
-                        global_points = spend_all_payer_points(global_points, transaction)
+            elif payers[transaction.payer] >= transaction.remaining_points:                         # if payer has more points than transaction
+                points_to_spend = spend_all_user_points(points_to_spend, transaction)
+                add_spent_points(transaction.payer, user_points)
+
+            elif payers[transaction.payer] >= points_to_spend:                                      # if payer has more points than there are left
+                points_to_spend = spend_all_user_points(points_to_spend, transaction)
+                add_spent_points(transaction.payer, user_points)
+
+            else:                                                                                   # if there are more points left than this payer has left
+                points_to_spend = spend_all_payer_points(points_to_spend, transaction)
+                add_spent_points(transaction.payer, payers[transaction.payer])
 
     spent_points_json = json.dumps(spent_points)
     return spent_points_json
@@ -143,8 +137,9 @@ def spend_all_user_points(points_to_spend: int, transaction: Transaction):
 
 def add_spent_points(payer: str, points: int):
     global spent_points
-    if payer in spent_points:
-        spent_points.payer -= points
-    else:
-        spent_points.update({"payer": payer, "points": 0 - points})
-    return spent_points
+
+    for item in spent_points:
+        if item["payer"] in item.keys():
+            item["points"] -= points
+        else:
+            spent_points.append({"payer": payer, "points": 0 - points})
