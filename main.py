fea438e6292fe3dc3ac7c38f0b68e18730efdcbd
Dalen Ledingham <dalen@Dalens-MacBook-Pro.local>
2021-08-29 15:44:42 -0500

    Started trimming nested if-else statements into individual functions

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class Transaction(BaseModel):
     points: int = 0
     timestamp: datetime
     spent: Optional[bool] = False
-    temp_points: Optional[int] = 0
+    remaining_points: Optional[int] = 0
 
 app = FastAPI()
 
@@ -26,24 +26,24 @@ transactions = []
 
 @app.get("/")
 async def root():
-    return user_points
+    return {"user points": user_points}
 
 
 # Payer Routes
 
-@app.post("/payer/")
+@app.post("/new-payer/")
 async def create_payer(payer: Payer):
     payers.update({payer.name: payer.points})
     return payer
 
-@app.get("/payer/", response_model=Dict[str, int])
+@app.get("/payers/", response_model=Dict[str, int])
 async def get_all_payers():
     return payers
 
 
 # Transaction Routes
 
-@app.post("/transaction/")
+@app.post("/new-transaction/")
 async def create_transaction(transaction: Transaction):
     try:
         if transaction.payer in payers.keys():
@@ -51,68 +51,100 @@ async def create_transaction(transaction: Transaction):
             transactions.append(transaction)
             payers[transaction.payer] += transaction.points
             user_points += transaction.points
-            transaction.temp_points = transaction.points
+            transaction.remaining_points = transaction.points
             return transaction
         else:
             raise HTTPException
     except:
         raise HTTPException(status_code=404, detail="Payer not found")
 
-@app.get("/transaction/", response_model=List[Transaction])
+@app.get("/transactions/", response_model=List[Transaction])
 async def get_all_transactions():
     return transactions
 
-
+global global_points
+global_points = 0
 # Spend Route
 
 @app.post("/spend/")
 async def spend(points_to_spend: int):
     global user_points
-    spent_points = []
+    global spent_points
+    global global_points
+    global_points = points_to_spend
+    spent_points = {}
     sorted_transactions = sorted(transactions, key=lambda x: x.timestamp) # sort transactions by timestamp
+
+    # if the user tries to spend more points than they have available
+    if points_to_spend > user_points:
+        raise HTTPException(status_code=400, detail="You do not have enough points")
+    
     # for each transaction in sorted_transactions
     for transaction in sorted_transactions:
         # if there are points left to spend
-        if points_to_spend > 0:
-            # if the current transaction's points are not all spent
-            if transaction.spent is False:
-                # if there are more points to spend than this transaction has available
-                if points_to_spend >= transaction.temp_points:
-                    # if this transaction's payer has more points than this transaction has available
-                    if payers[transaction.payer] >= transaction.temp_points:
-                        payers[transaction.payer] -= transaction.temp_points
-                        points_to_spend -= transaction.temp_points
-                        user_points -= transaction.temp_points
-                        transaction.temp_points = 0
-                        transaction.spent = True
-                    # if this transaction has more points than its payer has available
-                    else:
-                        transaction.temp_points -= payers[transaction.payer]
-                        points_to_spend -= payers[transaction.payer]
-                        user_points -= payers[transaction.payer]
-                        payers[transaction.payer] = 0
+        if global_points > 0 and transaction.spent is False:
+        
+            # if there are more points to spend than this transaction has available
+            if global_points >= transaction.remaining_points:
+                # if this transaction's payer has more points than this transaction has available - SPEND ALL TRANSACTION
+                if payers[transaction.payer] >= transaction.remaining_points:
+                    global_points = spend_all_transaction_points(global_points, transaction)
+
+                # if this transaction has more points than its payer has available - SPEND ALL PAYER
+                else:
+                    global_points = spend_all_payer_points(global_points, transaction)
+
+            # if this transaction has more points than there are left to spend
+            else:
+                # if this transactin's payer has more points than this transaction has available - SPEND ALL REMAINING
+                if payers[transaction.payer] >= transaction.remaining_points:
+                    global_points = spend_all_user_points(global_points, transaction)
+
                 # if this transaction has more points than there are left to spend
                 else:
-                    # if this transactin's payer has more points than this transaction has available
-                    if payers[transaction.payer] >= transaction.temp_points:
-                        transaction.temp_points -= points_to_spend
-                        payers[transaction.payer] -= points_to_spend
-                        user_points -= points_to_spend
-                        points_to_spend = 0
-                    # if this transaction has more points than there are left to spend
+                    # if this transaction's payer has more points than there are left to spend - SPEND ALL REMAINING
+                    if payers[transaction.payer] >= global_points:
+                        global_points = spend_all_user_points(global_points, transaction)
+
+                    # if there are more points left to spend than this transaction's payer has available - SPEND ALL PAYER
                     else:
-                        # if this transaction's payer has more points than there are left to spend
-                        if payers[transaction.payer] >= points_to_spend:
-                            transaction.temp_points -= points_to_spend
-                            payers[transaction.payer] -= points_to_spend
-                            user_points -= points_to_spend
-                            points_to_spend = 0
-                        # if there are more points left to spend than this transaction's payer has available
-                        else:
-                            transaction.temp_points -= payers[transaction.payer]
-                            points_to_spend -= payers[transaction.payer]
-                            user_points -= payers[transaction.payer]
-                            payers[transaction.payer] = 0
+                        global_points = spend_all_payer_points(global_points, transaction)
+
     spent_points_json = json.dumps(spent_points)
     return spent_points_json
 
+
+# Functions
+
+def spend_all_transaction_points(points_to_spend: int, transaction: Transaction):
+    global user_points
+    payers[transaction.payer] -= transaction.remaining_points
+    points_to_spend -= transaction.remaining_points
+    user_points -= transaction.remaining_points
+    transaction.remaining_points = 0
+    transaction.spent = True
+    return points_to_spend
+
+def spend_all_payer_points(points_to_spend: int, transaction: Transaction):
+    global user_points
+    transaction.remaining_points -= payers[transaction.payer]
+    points_to_spend -= payers[transaction.payer]
+    user_points -= payers[transaction.payer]
+    payers[transaction.payer] = 0
+    return points_to_spend
+
+def spend_all_user_points(points_to_spend: int, transaction: Transaction):
+    global user_points
+    transaction.remaining_points -= points_to_spend
+    payers[transaction.payer] -= points_to_spend
+    user_points -= points_to_spend
+    points_to_spend = 0
+    return points_to_spend
+
+def add_spent_points(payer: str, points: int):
+    global spent_points
+    if payer in spent_points:
+        spent_points.payer -= points
+    else:
+        spent_points.update({"payer": payer, "points": 0 - points})
+    return spent_points
