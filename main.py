482055116fa5a93f918db2519d772b0dad14761a
Dalen Ledingham <dalen@Dalens-MacBook-Pro.local>
2021-08-29 12:27:02 -0500

    Spend function calculates properly, need to make it return JSON data of points spent

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from typing import List, Dict
+from typing import List, Dict, Optional
 from datetime import datetime
+import json
 
 class Payer(BaseModel):
     name: str
     points: int = 0
 
 class Transaction(BaseModel):
-    id: int
     payer: str
     points: int = 0
     timestamp: datetime
-    spent: bool = False
+    spent: Optional[bool] = False
+    temp_points: Optional[int] = 0
 
 app = FastAPI()
 
@@ -20,11 +21,15 @@ user_points = 0
 payers = {}
 transactions = []
 
+
+# Root Route
+
 @app.get("/")
 async def root():
     return user_points
 
-# CRUD Payer
+
+# Payer Routes
 
 @app.post("/payer/")
 async def create_payer(payer: Payer):
@@ -35,45 +40,18 @@ async def create_payer(payer: Payer):
 async def get_all_payers():
     return payers
 
-# @app.get("/payer/{name}")
-# async def get_payer(name: str):
-#     try:
-#         return payers.get(name)
-#     except:
-#         raise HTTPException(status_code=404, detail="Payer not found")
 
-# @app.put("/payer/{name}")
-# async def update_payer(name: str, points: int):
-#     try:
-#         if name in payers.keys():
-#             payers[name] = points
-#             return payers[name]
-#         else: 
-#             raise HTTPException
-#     except:
-#         raise HTTPException(status_code=404, detail="Payer not found")
-
-# @app.delete("/payer/{name}")
-# async def delete_payer(name: str):
-#     try:
-#         if name in payers.keys():
-#             obj = payers[name]
-#             del payers[name]
-#             return obj
-#         else:
-#             raise HTTPException
-#     except:
-#         raise HTTPException(status_code=404, detail="Payer not found")
-
-# CRUD Transaction
+# Transaction Routes
 
 @app.post("/transaction/")
 async def create_transaction(transaction: Transaction):
     try:
         if transaction.payer in payers.keys():
+            global user_points
             transactions.append(transaction)
             payers[transaction.payer] += transaction.points
-            # user_points += transaction.points
+            user_points += transaction.points
+            transaction.temp_points = transaction.points
             return transaction
         else:
             raise HTTPException
@@ -84,54 +62,57 @@ async def create_transaction(transaction: Transaction):
 async def get_all_transactions():
     return transactions
 
-# @app.get("/transaction/{id}")
-# async def get_transaction(id: int):
-#     try:
-#         return transactions[id]
-#     except:
-#         raise HTTPException(status_code=404, detail="Transaction not found")
 
-# @app.put("/transaction/{id}")
-# async def update_transaction(id: int, transaction: Transaction):
-#     try:
-#         transactions[id] = transaction
-#         return transactions[id]
-#     except:
-#         raise HTTPException(status_code=404, detail="Transaction not found")
-
-# @app.delete("/transaction/{id}")
-# async def delete_transaction(id: int):
-#     try:
-#         obj = transactions[id]
-#         transactions.pop(id)
-#         return obj
-#     except:
-#         raise HTTPException(status_code=404, detail="Transaction not found")
+# Spend Route
 
 @app.post("/spend/")
 async def spend(points_to_spend: int):
+    global user_points
+    spent_points = []
     sorted_transactions = sorted(transactions, key=lambda x: x.timestamp) # sort transactions by timestamp
+    # for each transaction in sorted_transactions
     for transaction in sorted_transactions:
-        # if points still left to spend
+        # if there are points left to spend
         if points_to_spend > 0:
-            # if spending all of this transaction's points will not make payer balance negative
-            if payers[transaction.payer] - transaction.points <= 0:
-                payers[transaction.payer] -= transaction.points # adjust payer balance
-                # user_points -= transaction.points # adjust user balance
-                points_to_spend -= transaction.points # adjust points left to spend
-                # if transaction has no points left to spend
-                if transaction.points == 0:
-                    transaction.spent = True
-            else:
-                # TODO: what to do if transaction's points would make payer balance negative
-                # TODO: what to do if transaction's points greater than points left to spend
-                break # placeholder
-        else:
-            continue
-    return sorted_transactions
-
-
-# track points balance per payer using dict
-# on spend, loop through sorted non-spent transactions and subtract spent points from transaction's payer
-# if transaction has negative points value, "spend" it by giving those points back to the payer
+            # if the current transaction's points are not all spent
+            if transaction.spent is False:
+                # if there are more points to spend than this transaction has available
+                if points_to_spend >= transaction.temp_points:
+                    # if this transaction's payer has more points than this transaction has available
+                    if payers[transaction.payer] >= transaction.temp_points:
+                        payers[transaction.payer] -= transaction.temp_points
+                        points_to_spend -= transaction.temp_points
+                        user_points -= transaction.temp_points
+                        transaction.temp_points = 0
+                        transaction.spent = True
+                    # if this transaction has more points than its payer has available
+                    else:
+                        transaction.temp_points -= payers[transaction.payer]
+                        points_to_spend -= payers[transaction.payer]
+                        user_points -= payers[transaction.payer]
+                        payers[transaction.payer] = 0
+                # if this transaction has more points than there are left to spend
+                else:
+                    # if this transactin's payer has more points than this transaction has available
+                    if payers[transaction.payer] >= transaction.temp_points:
+                        transaction.temp_points -= points_to_spend
+                        payers[transaction.payer] -= points_to_spend
+                        user_points -= points_to_spend
+                        points_to_spend = 0
+                    # if this transaction has more points than there are left to spend
+                    else:
+                        # if this transaction's payer has more points than there are left to spend
+                        if payers[transaction.payer] >= points_to_spend:
+                            transaction.temp_points -= points_to_spend
+                            payers[transaction.payer] -= points_to_spend
+                            user_points -= points_to_spend
+                            points_to_spend = 0
+                        # if there are more points left to spend than this transaction's payer has available
+                        else:
+                            transaction.temp_points -= payers[transaction.payer]
+                            points_to_spend -= payers[transaction.payer]
+                            user_points -= payers[transaction.payer]
+                            payers[transaction.payer] = 0
+    spent_points_json = json.dumps(spent_points)
+    return spent_points_json
 
